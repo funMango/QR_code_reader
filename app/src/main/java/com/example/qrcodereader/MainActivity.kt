a51ba7063d645683ccdbb4ff8c62309a7a0d4850
funMango <bluemango608@gmail.com>
2023-07-07 18:52:17 +0900

    기능: 카메라 미리보기 기능 구현하기

--- app/src/main/java/com/example/qrcodereader/MainActivity.kt
+++ app/src/main/java/com/example/qrcodereader/MainActivity.kt
@@ -2,10 +2,41 @@ package com.example.qrcodereader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
+import com.example.qrcodereader.databinding.ActivityMainBinding
+import com.google.common.util.concurrent.ListenableFuture
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+    private lateinit var cameraProviderFuture : ListenableFuture<ProcessCameraProvider>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        startCamera()
+    }
+
+    fun startCamera() {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider = cameraProviderFuture.get()
+
+            val preview = getPreview() // 미리보기 객체
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA // 후면카메라 선택
+            cameraProvider.bindToLifecycle(this, cameraSelector, preview) // 미리보기 기능 선택
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    fun getPreview() : Preview {
+        val preview: Preview = Preview.Builder().build()
+        preview.setSurfaceProvider(binding.barcodePreview.surfaceProvider)
+
+        return preview
     }
 }
